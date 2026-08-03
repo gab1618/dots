1d341d8adb3724882b007a8cf21273f7d5d2d996
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2026-08-03 19:01:28 -0300

    hypr: fix firefox PIP
    Also, fix some indents

--- src/.config/hypr/confs/windows.lua
+++ src/.config/hypr/confs/windows.lua
@@ -1,30 +1,38 @@
 local suppressMaximizeRule = hl.window_rule({
-    name  = "suppress-maximize-events",
-    match = { class = ".*" },
+  name  = "suppress-maximize-events",
+  match = { class = ".*" },
 
-    suppress_event = "maximize",
+  suppress_event = "maximize",
 })
 
 hl.window_rule({
-    -- Fix some dragging issues with XWayland
-    name  = "fix-xwayland-drags",
-    match = {
-        class      = "^$",
-        title      = "^$",
-        xwayland   = true,
-        float      = true,
-        fullscreen = false,
-        pin        = false,
-    },
+  -- Fix some dragging issues with XWayland
+  name  = "fix-xwayland-drags",
+  match = {
+    class      = "^$",
+    title      = "^$",
+    xwayland   = true,
+    float      = true,
+    fullscreen = false,
+    pin        = false,
+  },
 
-    no_focus = true,
+  no_focus = true,
 })
 
--- Hyprland-run windowrule
 hl.window_rule({
-    name  = "move-hyprland-run",
-    match = { class = "hyprland-run" },
+  name  = "move-hyprland-run",
+  match = { class = "hyprland-run" },
 
-    move  = "20 monitor_h-120",
-    float = true,
+  move  = "20 monitor_h-120",
+  float = true,
+})
+
+hl.window_rule({
+  name = "fix-firefox-pip",
+  match = {
+    title = "^(Picture-in-Picture|Firefox)$"
+  },
+  float = true,
+  pin = true
 })
