60b7c78d9a44c5f0055fc083b052e3d0e857c6c4
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2026-08-11 11:55:00 -0300

    hypr: autostart fcitx

--- src/.config/hypr/confs/autostart.lua
+++ src/.config/hypr/confs/autostart.lua
@@ -2,6 +2,6 @@ local vars = require("confs.vars")
 
 hl.on("hyprland.start", function ()
   hl.exec_cmd(vars.terminal)
-  hl.exec_cmd("qs & waybar & hypridle & hyprpaper & walker --gapplication-service & elephant & swayosd-server")
+  hl.exec_cmd("fcitx5 -d & qs & waybar & hypridle & hyprpaper & walker --gapplication-service & elephant & swayosd-server")
 end)
 
