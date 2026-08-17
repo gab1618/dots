b8ee7558d923e48a7d6586fcc73648dbb2975035
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2026-08-17 19:29:05 -0300

    hypr: setup dark mode commands

--- src/.config/hypr/confs/autostart.lua
+++ src/.config/hypr/confs/autostart.lua
@@ -3,5 +3,6 @@ local vars = require("confs.vars")
 hl.on("hyprland.start", function ()
   hl.exec_cmd(vars.terminal)
   hl.exec_cmd("fcitx5 -d & qs & waybar & hypridle & hyprpaper & walker --gapplication-service & elephant & swayosd-server")
+  hl.exec_cmd("gsettings get org.gnome.desktop.interface color-scheme && gsettings set org.gnome.desktop.interface color-scheme prefer-dark")
 end)
 
